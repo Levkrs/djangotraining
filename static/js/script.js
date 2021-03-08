08283cdde6c8edcad538d3243248ce967231423d
SKov82 <skov82@ya.ru>
2021-03-08 22:09:56 +0300

    скрипт анимация счетчиков вакансий, резюме, компаний, соискателей, небольшая правка стилей

--- static/js/script.js
+++ static/js/script.js
@@ -37,6 +37,7 @@ buttonToTop.addEventListener('click', () => {
 
 // End BackToTop button
 
+
 // BurgerMenu
 
 const menuWrapper = document.querySelector('.nav-on-header'),
@@ -57,3 +58,34 @@ menu.addEventListener('click', (event) => {
 })
 
 // End BurgerMenu
+
+
+// CounterAnimation
+
+class Counter {
+    constructor($el, targetCount, duration = 2000, stepInterval = 100) {
+        this.$el = $el
+        this.currentCount = 0
+        this.targetCount = Number(targetCount)
+        this.stepCount = +(targetCount / (+duration / stepInterval)).toFixed(0)
+        this.stepInterval = +stepInterval
+        this.interval = null
+    }
+
+    init() {
+        this.interval = setInterval(() => {
+            if (this.currentCount > this.targetCount) {
+                this.currentCount = this.targetCount
+                clearInterval(this.interval)
+            }
+            this.$el.textContent = this.currentCount
+            this.currentCount += this.stepCount
+        }, this.stepInterval)
+    }
+}
+
+document.querySelectorAll('.counter p span').forEach(($el) => {
+    new Counter($el, $el.textContent, $el.dataset.time, $el.dataset.step).init()
+})
+
+// End CounterAnimation
