3c8b28bb0792f01823cb2c99d0b68a1d1e11cc11
levhotylev <levkrs@yandex.ru>
2021-03-10 13:58:36 +0700

    Create resume

--- applicantapp/urls.py
+++ applicantapp/urls.py
@@ -3,7 +3,7 @@ Urls for applicant
 """
 from django.urls import path
 
-from applicantapp.views import ProfileView, ResumeList
+from applicantapp.views import ProfileView, ResumeList, CreateResume
 
 
 app_name = 'applicantapp'
@@ -11,4 +11,5 @@ app_name = 'applicantapp'
 urlpatterns = [
     path('<int:pk>/profile/', ProfileView.as_view(), name='profile'),
     path('resumes', ResumeList.as_view(), name='resumes'),
+    path('createresume', CreateResume.as_view(), name='createresume'),
 ]
