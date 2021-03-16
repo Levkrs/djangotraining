55ff91af09c76117b7327502e87208a5be0673ee
SKov82 <skov82@ya.ru>
2021-03-17 00:16:59 +0300

    организовал селекты в шаблонах циклами, добавил фон в ЛК соискателя

--- companyapp/urls.py
+++ companyapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('<int:pk>/profile/', ProfileView.as_view(), name='profile'),
     path('<int:pk>/card/', CompanyDetailView.as_view(), name='card'),
     path('<int:pk>/card/edit/', CompanyUpdateView.as_view(), name='edit'),
-    path('<int:pk>/job/new/', JobCreateView.as_view(), name='new_job'),
-    path('<int:pk>/job/<int:job_pk>/edit/', JobUpdateView.as_view(), name='edit_job'),
-    path('job-list', JobListView.as_view(), name='job-list'),
+    path('<int:pk>/job/new/', JobCreateView.as_view(), name='new-job'),
+    path('job/<int:pk>/edit/', JobUpdateView.as_view(), name='edit-job'),
+    path('job-list/', JobListView.as_view(), name='job-list'),
 ]
