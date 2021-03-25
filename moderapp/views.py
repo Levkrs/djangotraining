da1c61b9e6c24815748f2a992648d39c31a0055f
levhotylev <levkrs@yandex.ru>
2021-03-25 12:15:29 +0700

    Detail aprove RESUME and Job v. Clear

--- moderapp/views.py
+++ moderapp/views.py
@@ -6,7 +6,6 @@ from django.views.generic import ListView, TemplateView, DetailView, UpdateView
 
 from applicantapp.models import Resume
 from companyapp.models import Job, Company
-from moderapp.forms import ResumeForChecked
 
 
 class ModerListPage(TemplateView):
@@ -21,36 +20,6 @@ class ModerListPage(TemplateView):
         context['company_for_aprove'] = Company.objects.filter(status=2)
 
         return context
-
-
-# class CheckedResumePage(DetailView):
-#     model = Resume
-#     # form_class = ResumeForChecked
-#     template_name = 'moderapp/moder_resume_detail.html'
-#     success_url = '/'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(CheckedResumePage, self).get_context_data(**kwargs)
-#         print('For aprove')
-#         return context
-#
-# class JobUpdateView(LoginRequiredMixin, UpdateView):
-#     """ Редактор карточки вакансии """
-#     model = Job
-#     fields = ('status', 'grade', 'category', 'salary', 'city', 'employment', 'skills',
-#               'work_schedule', 'experience', 'short_description', 'description',)
-
-# class CheckedResumePage(UpdateView):
-#     """
-#     Checked Resume
-#     """
-#     model = Resume
-#     form_class = ResumeForChecked
-#     template_name = 'moderapp/moder_resume_detail.html'
-#     success_url = '/'
-#
-#     # def get_success_url(self):
-    #     return reverse_lazy('companyapp:card', args=[self.object.pk])
 
 class CheckedResumePage(UpdateView):
     model = Resume
