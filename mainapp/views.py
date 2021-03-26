65f205ac67f22fb68c9cbe5baf8d6228cda8ac3f
Vladyslav-tech <vladyslavvovk@yandex.ru>
2021-03-27 00:58:47 +0300

    добавил поиск вакансий по краткому описанию

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Q
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from companyapp.models import Job
 
 
 class Index(TemplateView):
@@ -8,4 +11,23 @@ class Index(TemplateView):
     """    
     template_name = 'mainapp/index.html'
     extra_context = {'title_name': 'GeekStaff'}
+
+
+class JobSearchList(ListView):
+    """Поиск вакансий по краткому описанию"""
+    model = Job
+    template_name = 'mainapp/job_search.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            object_list = Job.objects.filter(
+                Q(description__icontains=query) | Q(short_description__icontains=query)
+            )
+            return object_list
+        else:
+            return Job.objects.all()
+
+
     
