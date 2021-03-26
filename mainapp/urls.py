ef8d73f20c717a2df650376e4ff45daf0990ea27
Vladyslav-tech <vladyslavvovk@yandex.ru>
2021-03-27 00:57:54 +0300

    добавил url поиска вакансий в нав меню

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import Index
+from .views import Index, JobSearchList
 
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', Index.as_view(), name='index')
+    path('', Index.as_view(), name='index'),
+    path('job_search/', JobSearchList.as_view(), name='job_search')
 ]
