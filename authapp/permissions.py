74dbc81bfbc8d7b6c7ab2196c3a1e8b3c18e3e5a
Vladyslav-tech <vladyslavvovk@yandex.ru>
2021-04-02 18:50:36 +0300

    права пользователей на основе роли пользователя

--- authapp/permissions.py
+++ authapp/permissions.py
@@ -1,26 +1,24 @@
 from django.http import Http404
 
-# Свои классы для предоставления прав на основе роли юзера
-# пока не используются
 
 PERMISSION_DENIED_MESSAGE = 'У вас нет прав просматривать эту страницу'
 
 class ApplicantPermissionMixin:
-    """Класс проверки прав доступа на основе роли для соискателя"""
+    """Класс выделения прав доступа на основе роли для соискателя"""
     def has_permission(self):
-        return self.request.user.role == 'REC'
+        return self.request.user.role == 'REC' or self.request.user.role == 'MOD'
 
     def dispatch(self, request, *args, **kwargs):
-        if self.has_permission():
-            raise Http404
+        if not self.has_permission():
+            raise Http404 # TODO заменить на 403 или своим шаблоном отказа в доступе
         return super().dispatch(request, *args, **kwargs)
 
 class CompanyPermissionMixin(ApplicantPermissionMixin):
-    """Класс проверки прав доступа на основе роли для работодателя"""
+    """Класс выделения прав доступа на основе роли для работодателя"""
     def has_permission(self):
-        return self.request.user.role == 'HR'
+        return self.request.user.role == 'HR' or self.request.user.role == 'MOD'
 
 class ModeratorPermissionMixin:
-    """Класс проверки прав доступа на основе роли для модератора"""
+    """Класс выделения прав доступа на основе роли для модератора"""
     def has_permission(self):
         return self.request.user.role == 'MOD'
