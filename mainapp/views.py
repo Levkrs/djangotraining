1d585efddc5c4bf384cae151f3f2878a7b791360
Vladyslav-tech <vladyslavvovk@yandex.ru>
2021-04-02 23:48:19 +0300

    удалил поиск из mainapp

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,22 +12,3 @@ class Index(TemplateView):
     template_name = 'mainapp/index.html'
     extra_context = {'title_name': 'GeekStaff'}
 
-
-class JobSearchList(ListView):
-    """Поиск вакансий по краткому описанию"""
-    model = Job
-    template_name = 'mainapp/job_search.html'
-    paginate_by = 10
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            object_list = Job.objects.filter(
-                Q(description__icontains=query) | Q(short_description__icontains=query)
-            )
-            return object_list
-        else:
-            return Job.objects.all()
-
-
-    
