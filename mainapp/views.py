238ef813ba771c5d8f28c8b0e227da312d14cd2a
Pavel Stefanenko <p.stefanenko@gmail.com>
2021-04-06 06:56:01 +0300

    Новсти в кабинете модератора

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,14 @@
-from django.db.models import Q
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView
 
-from companyapp.models import Job
+from moderapp.models import News
 
 
 class Index(TemplateView):
     """
     Главная страница
-    """    
+    """
     template_name = 'mainapp/index.html'
-    extra_context = {'title_name': 'GeekStaff'}
-
+    extra_context = {
+        'title_name': 'GeekStaff',
+        'news': News.objects.all().order_by('-created_at'),
+    }
--- moderapp/views.py
+++ moderapp/views.py
@@ -1,35 +1,46 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.shortcuts import render
-
-# Create your views here.
-from django.urls import reverse
-from django.views.generic import ListView, TemplateView, DetailView, UpdateView
-from django.views.generic import ListView, TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, UpdateView, CreateView
 
 from applicantapp.models import Resume
 from companyapp.models import Job, Company
-from moderapp.forms import ResumeModerateForm, JobModerateForm, CompanyModerateForm
+from moderapp.forms import ResumeModerateForm, JobModerateForm, CompanyModerateForm, AddUpdateNewsForm
 from authapp.permissions import ModeratorPermissionMixin
+from moderapp.models import News
 
 
 class ModerListPage(LoginRequiredMixin, ModeratorPermissionMixin, TemplateView):
+    """
+    Moderator main page
+    """
     template_name = 'moderapp/moder_main_page.html'
 
     def get_context_data(self, **kwargs):
         context = super(ModerListPage, self).get_context_data(**kwargs)
+        context['news'] = News.objects.all().order_by('-created_at')
         context['resume_for_aprove'] = Resume.objects.filter(status=2)
         context['job_for_aprove'] = Job.objects.filter(status=2)
         context['company_for_aprove'] = Company.objects.filter(status=2)
         return context
 
-class CheckedResumePage(UpdateView):
-    model = Resume
-    fields = '__all__'
-    template_name = 'moderapp/moder_resume_detail.html'
-    # success_url = '/'
 
-    def get_success_url(self):
-        return reverse('moderapp:moder_list_page')
+class AddNews(LoginRequiredMixin, ModeratorPermissionMixin, CreateView):
+    """
+    Adding news
+    """
+    model = News
+    form_class = AddUpdateNewsForm
+    template_name = 'moderapp/add_news.html'
+    success_url = '/moder'
+
+
+class UpdateNews(LoginRequiredMixin, ModeratorPermissionMixin, UpdateView):
+    """
+    Read news details and update
+    """
+    model = News
+    form_class = AddUpdateNewsForm
+    template_name = 'moderapp/obj_datail.html'
+    success_url = '/moder'
 
 
 class ModerateResume(LoginRequiredMixin, ModeratorPermissionMixin, UpdateView):
@@ -60,13 +71,3 @@ class ModerateCompany(LoginRequiredMixin, ModeratorPermissionMixin, UpdateView):
     form_class = CompanyModerateForm
     template_name = 'moderapp/obj_datail.html'
     success_url = '/moder'
-
-
-
-class CheckJobPage(UpdateView):
-    model = Job
-    fields = '__all__'
-    template_name = 'moderapp/moder_job_detail.html'
-
-    def get_success_url(self):
-        return reverse('moderapp:moder_list_page')
