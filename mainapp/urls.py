29ef05374302d4237441a74862628755fdf8acd8
Pavel Stefanenko <p.stefanenko@gmail.com>
2021-04-08 07:35:23 +0300

    Новости на главной

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import Index
-
+from .views import Index, AllNews, NewsDetail
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('news/', AllNews.as_view(), name='news_all'),
+    path('news/<int:pk>/', NewsDetail.as_view(), name='news_detail'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from moderapp.models import News
 
@@ -10,5 +10,23 @@ class Index(TemplateView):
     template_name = 'mainapp/index.html'
     extra_context = {
         'title_name': 'GeekStaff',
-        'news': News.objects.all().order_by('-created_at'),
+        'news': News.objects.all().order_by('-created_at')[:5],
     }
+
+
+class AllNews(ListView):
+    """
+    All news
+    """
+    template_name = 'mainapp/news_all.html'
+
+    def get_queryset(self):
+        return News.objects.all().order_by('-created_at')
+
+
+class NewsDetail(DetailView):
+    """
+    Detail of some news item
+    """
+    model = News
+    template_name = 'mainapp/news_detail.html'
